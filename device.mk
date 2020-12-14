e7852188ca64f2eed0ec4a63ad8fd5f8456ac780
JamieHoSzeYui <henloboii@protonmail.ch>
2020-12-14 10:05:54 +0800

    lancelot : don't build mtk telephony jars for now

--- device.mk
+++ device.mk
@@ -61,11 +61,11 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 # Telephony Jars
-PRODUCT_BOOT_JARS += \
-    mediatek-common \
-    mediatek-framework \
-    mediatek-ims-base \
-    mediatek-ims-common \
-    mediatek-telecom-common \
-    mediatek-telephony-base \
-    mediatek-telephony-common
+# PRODUCT_BOOT_JARS += \
+#     mediatek-common \
+#     mediatek-framework \
+#     mediatek-ims-base \
+#     mediatek-ims-common \
+#     mediatek-telecom-common \
+#     mediatek-telephony-base \
+#     mediatek-telephony-common
